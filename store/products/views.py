fe9330964fb55f249963372a5e704dfaddd8f79e
noobprofesor <noobprofesor@gmail.com>
2025-01-22 22:23:51 +0300

    add load data from DB

--- store/products/views.py
+++ store/products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
 def index(request):
     return render(request,'products/index.html')
 
 def products(request):
-    return render(request,'products/products.html')
+    context = {
+        'title':'store - catalog',
+        'products': Product.objects.all(),
+        'categorys':ProductCategory.objects.all()
+    }
+    return render(request,'products/products.html', context)
