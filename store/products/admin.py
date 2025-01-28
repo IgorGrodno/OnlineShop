49b5e3a56ad921d9a14af553e7232ae757495ab2
noobprofesor <noobprofesor@gmail.com>
2025-01-28 18:10:10 +0300

    add classes for admin panel

--- store/products/admin.py
+++ store/products/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from products.models import Product, ProductCategory, Basket
 
-admin.site.register(Product)
+
 admin.site.register(ProductCategory)
 admin.site.register(Basket)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','quantity','category']
+    fields = ['name','description','price','quantity','image','category']
+    search_fields = ['name']
+    ordering = ['-name']
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ['product','quantity']
+    extra=0
