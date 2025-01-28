115d50be2b1a084e7b1e7007c3886fa09a84188f
noobprofesor <noobprofesor@gmail.com>
2025-01-28 17:46:24 +0300

    add pagination and category filter

--- store/products/views.py
+++ store/products/views.py
@@ -4,14 +4,22 @@ from django.shortcuts import render
 from products.models import Product, ProductCategory, Basket
 from users.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def index(request):
     return render(request,'products/index.html')
 
-def products(request):
+def products(request, category_id=None, page_number=1):   
+    if category_id:
+        products = Product.objects.filter(category__id=category_id)
+    else:
+        products=Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
     context = {
         'title':'store - catalog',
-        'products': Product.objects.all(),
+        'products': products_paginator,
         'categorys':ProductCategory.objects.all()
     }
     return render(request,'products/products.html', context)
