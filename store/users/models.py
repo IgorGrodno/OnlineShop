36c952133e39d143758f7c06173e685a4457d676
noobprofesor <noobprofesor@gmail.com>
2025-01-30 13:08:10 +0300

    files add

--- store/users/models.py
+++ store/users/models.py
@@ -1,19 +1,20 @@
-from django.db import models
-from django.core.mail import send_mail
-from django.contrib.auth.models import AbstractUser
-from django.urls import reverse
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
+from django.db import models
+from django.urls import reverse
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
     is_verified_email = models.BooleanField(default=False)
 
 
 class EmailVerification(models.Model):
     code = models.UUIDField(unique=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    created = models.DateField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     expiration = models.DateTimeField()
 
     def __str__(self):
@@ -22,8 +23,11 @@ class EmailVerification(models.Model):
     def send_verification_email(self):
         link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.code})
         verification_link = f'{settings.DOMAIN_NAME}{link}'
-        subject = f'Подтверждение учетной записи для {self.user.username}'
-        message = f'Для подтверждения адреса электронной почты для {self.user.email} перейдите по ссылке {verification_link}'
+        subject = f'Подверждение учетной записи для {self.user.username}'
+        message = 'Для подверждения учетной записи для {} перейдите по ссылке: {}'.format(
+            self.user.email,
+            verification_link
+        )
         send_mail(
             subject=subject,
             message=message,
@@ -31,3 +35,6 @@ class EmailVerification(models.Model):
             recipient_list=[self.user.email],
             fail_silently=False,
         )
+
+    def is_expired(self):
+        return True if now() >= self.expiration else False
